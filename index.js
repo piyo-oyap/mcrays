7b7963e0c1f320a02c1ac67be7fca7f5a77f0ff1
crs26 <christianrosales.salvadora@bicol-u.edu.ph>
2020-02-11 10:59:14 +0800

    update device status checking

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var server = require('ws').Server;
 var wss = new server({port: 5001});
 var device;
+var device_status = false;
 
 wss.on('connection', function(ws){
 
@@ -12,7 +13,8 @@ wss.on('connection', function(ws){
 			if(in_data.type=='connection'){
 				if(in_data.content=='device'){
 					device = ws;
-					ws.on('close', function(){
+					device.on('pong', heartbeat);
+					device.on('close', function(){
 						wss.clients.forEach(function each(client){
 							if (client !== ws) {
 								client.send(JSON.stringify({
@@ -55,6 +57,11 @@ wss.on('connection', function(ws){
 				wss.clients.forEach(function each(client){
 						client.send(msg);
 				});
+
+				if(in_data.content == 'off'){
+					device.close();
+					console.log('shutting down the device');
+				}
 			}	
 		} catch (error) {
 			console.log(error.message);
@@ -62,3 +69,26 @@ wss.on('connection', function(ws){
 		
 	})
 });
+
+setInterval(function(){
+	if (device != null) {
+		device.ping();
+		if(device.isAlive ==false && device_status != device.isAlive){
+			wss.clients.forEach(function each(client){
+				if (client !== device) {
+					client.send(JSON.stringify({
+						type: 'connection',
+						content: 'device_offline'
+					}));
+				}
+			});
+			return device.terminate();
+		}
+		device.isAlive=false;
+		device_status = device.isAlive;
+	}
+},3000);
+
+function heartbeat(){
+	this.isAlive = true;
+}
